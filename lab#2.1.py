e9ef8949c503e3af0fc40adfca283f10afe6d1c1
al-mansur024 <al.mansur024@gmail.com>
2025-10-23 23:29:31 +0400

    Added task 2

--- lab#2.1.py
+++ lab#2.1.py
@@ -1,11 +1,12 @@
 # Напиши программу, которая принимает два числа и
 # выводит меньшее из них (использовать условие if).
+
 a = int(input("Enter your number: "))
 b = int(input("Enter your number: "))
 
-if (a > b):
-    print("Smaller number = ", b)
-elif (b > a):
+if (a < b):
     print("Smaller number = ", a)
+elif (a > b):
+    print("Smaller number = ", b)
 else:
     print("Numbers are equal!")
